3373698b4677f0a631fb9e6979519d88fdd52c2a
jhorman9 <jhosmita9@gmail.com>
2022-11-03 14:00:57 -0500

    actualizando

--- component/CountryItems.jsx
+++ component/CountryItems.jsx
@@ -4,7 +4,7 @@ const CountryItems = ({country}) => {
     return (
         <div className='countries'>
             <div style={{marginBottom:"5px"}}><b>{country.altSpellings[1] === undefined ? country.name.official : country.altSpellings[1]}</b></div>
-            <img src={country.flags.svg}/>
+            <img src={country.flags.svg} alt={`flag of ${country.name.common}`}/>
             <div className='margin'><b>Country: </b>{country.name.common}</div>
             <div className='margin'><b>Capital: </b>{country.capital?.[0]}</div>
             <div className='margin'><b>Continent: </b>{country.continents[0]}</div>
